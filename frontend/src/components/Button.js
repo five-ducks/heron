08ef334124a1e79ecd95339acc86c2e2a2683171
mingkyeongg <ming0820@naver.com>
2024-08-20 22:16:51 +0900

    Feature: Login page 버튼 만들기

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -12,15 +12,17 @@ export class Button extends Component {
 			height = '246.25px',
 			background = "url('../public/images/button.png')",
 			color = 'white',
+			size = '100px',
 		} = style;
 
 		this.el.style.width = width;
 		this.el.style.height = height;
+		this.el.style.backgroundSize = '100% 100%';
 		this.el.style.backgroundImage = background;
 		this.el.style.color = color;
-		this.el.style.backgroundSize = 'cover';
 		this.el.style.border = 'none';
 		this.el.style.backgroundColor = 'transparent';
+		this.el.style.fontSize = size;
 		this.render();
 		this.el.addEventListener('click', onClick);
 	}
