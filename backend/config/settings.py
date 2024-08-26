70d1b9e23cb7c4e4cf3fd539917401c07dbeff85
leesiha <mlice1030@soongsil.ac.kr>
2024-08-26 17:34:36 +0900

    chore: Add JSONRenderer as default renderer for REST framework
    The `DEFAULT_RENDERER_CLASSES` setting in the `REST_FRAMEWORK` configuration is updated to include the `JSONRenderer` class. This ensures that JSON is used as the default renderer for API responses.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'drf_spectacular',
 ]
 
@@ -50,7 +49,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 SPECTACULAR_SETTINGS = {
