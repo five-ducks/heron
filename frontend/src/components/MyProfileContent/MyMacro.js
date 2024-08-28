885afd262e2a0b9a25fd4ff8218560485524e375
mingkyeongg <ming0820@naver.com>
2024-08-28 10:34:40 +0900

    Design: 매크로 영역 디자인 수정

--- frontend/src/components/MyProfileContent/MyMacro.js
+++ frontend/src/components/MyProfileContent/MyMacro.js
@@ -11,41 +11,41 @@ export class MyMacro extends Component {
 	}
 	render() {
 		this.el.innerHTML = /*html*/`
-			<div class="macro-input f1">F1</div>
-			<div class="macro-input f2">F2</div>
-			<div class="macro-input f3">F3</div>
-			<div class="macro-input f4">F4</div>
-			<div class="macro-input f5">F5</div>
+			<div class="macro-input f1"><span>F1 : </span></div>
+			<div class="macro-input f2"><span>F2 : </span></div>
+			<div class="macro-input f3"><span>F3 : </span></div>
+			<div class="macro-input f4"><span>F4 : </span></div>
+			<div class="macro-input f5"><span>F5 : </span></div>
 		`
 		this.el.querySelector('.f1').appendChild(new Input(
 			'', 'text', {
 				background: "url('../../../public/images/ui/macro-input.png')",
 				width: '320px',
 				height: '50px',
-			}, 'hi').el);
+			}, 'Hi').el);
 		this.el.querySelector('.f2').appendChild(new Input(
 			'', 'text', {
 				background: "url('../../../public/images/ui/macro-input.png')",
 				width: '320px',
 				height: '50px',
-			}, 'hi').el);
+			}, 'GG').el);
 		this.el.querySelector('.f3').appendChild(new Input(
 			'', 'text', {
 				background: "url('../../../public/images/ui/macro-input.png')",
 				width: '320px',
 				height: '50px',
-			}, 'hi').el);
+			}, '좋은 게임이였습니다.').el);
 		this.el.querySelector('.f4').appendChild(new Input(
 			'', 'text', {
 				background: "url('../../../public/images/ui/macro-input.png')",
 				width: '320px',
 				height: '50px',
-			}, 'hi').el);
+			}, 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋ').el);
 		this.el.querySelector('.f5').appendChild(new Input(
 			'', 'text', {
 				background: "url('../../../public/images/ui/macro-input.png')",
 				width: '320px',
 				height: '50px',
-			}, 'hi').el);
+			}, ':)').el);
 	}
 }
