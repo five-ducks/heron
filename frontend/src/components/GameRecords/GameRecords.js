995a14153ffc425766b08f09fbcc9315da2bd42e
mingkyeongg <ming0820@naver.com>
2024-08-28 13:18:16 +0900

    Feature: 게임 전적 데이터 불러오기 비동기로 수정

--- frontend/src/components/GameRecords/GameRecords.js
+++ frontend/src/components/GameRecords/GameRecords.js
@@ -1,4 +1,5 @@
 import { Component } from "../../core/core.js";
+import { OneGameResult } from "./OneGameResult.js";
 
 // 전적 영역
 export class GameRecords extends Component {
@@ -8,14 +9,29 @@ export class GameRecords extends Component {
 				className: 'game-records',
 			}
 		});
-		// 데이터 불러오기
-		fetch('https://localhost/src/temp/gameRecords.json')
-		.then(res => res.json())
-		.then(gameRecords => {
-			console.log(gameRecords);
-		})
-		.catch(error => {
-			console.error('Error:', error);
+	}
+	// 데이터 불러오기
+	// 비동기로 수정
+	async loadData() {
+		try {
+			const response = await fetch('https://localhost/src/temp/gameRecords.json');
+			const data = await response.json();
+			this.gameRecords = data.gameRecords;
+			console.log('gameRecords:', this.gameRecords);
+		} catch (error) {
+			console.error('Error loading game records:', error);
+		}
+	}
+	
+	async render() {
+		this.el.innerHTML = /*html*/`
+		`
+
+		// 데이터 불러오기까지 기다림
+		await this.loadData();
+
+		this.gameRecords.forEach(gameResult => {
+			this.el.appendChild(new OneGameResult(gameResult).el);
 		});
 	}
 }
--- frontend/src/components/GameRecords/OneGameResult.js
+++ frontend/src/components/GameRecords/OneGameResult.js
@@ -3,12 +3,14 @@ import { Component } from "../../core/core.js";
 // 하나의 게임 결과
 
 export class OneGameResult extends Component {
-	constructor() {
+	constructor(gameResult) {
 		super({
 			props: {
 				className: 'one-game-result',
 			}
 		});
+		this.gameResult = gameResult;
+		console.log(this.gameResult);
 	}
 
 	render() {
