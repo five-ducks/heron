03ee94949755801a6aa5824cd545bbbb4a5725ad
nuyheat <taehkim2@student.42seoul.kr>
2024-08-29 13:58:55 +0900

    refactor: users login api / chore: django superuser create

--- backend/users/views.py
+++ backend/users/views.py
@@ -120,21 +120,18 @@ class UserViewSet(viewsets.ViewSet):
         responses={
             200: OpenApiResponse(description="User logged in successfully"),
             400: OpenApiResponse(description="Bad request"),
-            401: OpenApiResponse(description="Unauthorized"),
         },
         tags=["User"]
     )
     @action(detail=False, methods=['post'])
     def login(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            # print(serializer.validated_data['username'], serializer.validated_data['password'])
-            user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
-            if user is not None:
+        if request.user.is_anonymous:
+            serializer = LoginSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.validated_data['user']
                 login(request, user)
                 return Response(status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     @extend_schema(
         summary="User logout",
