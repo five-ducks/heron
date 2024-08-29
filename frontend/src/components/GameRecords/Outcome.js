103c660e11a51d22156989139fca901e4494af9a
mingkyeongg <ming0820@naver.com>
2024-08-29 14:37:21 +0900

    Refactor: 나의 전적 기능 & UI 개선

--- frontend/src/components/GameRecords/Outcome.js
+++ frontend/src/components/GameRecords/Outcome.js
@@ -1,11 +1,25 @@
 import { Component } from "../../core/core.js";
 
 export class Outcome extends Component {
+	constructor() {
+		super({
+			props: {
+				className: 'outcome',
+			}
+		});
+	}
 	render(isWin, userName) {
+
+		const result = isWin ? 'WIN' : 'LOSE';
+		
 		this.el.innerHTML = /*html*/`
-			<div class="profile-result-img"></div>
+			<div class="profile-container">
+				<div class="profile-result-img"></div>
+				<div class="user-name">${userName}</div>
+			</div>
 			<div class="result">
-				${isWin ? 'WIN' : 'LOSE'}
+				${result}
+			</div>
 		`
 		const profileImg = this.el.querySelector('.profile-result-img'); 
 		const img = document.createElement('img');
@@ -13,10 +27,17 @@ export class Outcome extends Component {
 		// 여기 userId로 이미지 찾는거 넣어야함
 		// 지금은 임시 이미지
 		profileImg.appendChild(img);
-		const profileName =  document.createElement('span');
-		// 여기 userId로 이름 찾는거 넣어야함
-		// 지금은 임시 이름
-		profileName.textContent = userName;
-		profileImg.appendChild(profileName);
+
+		console.log(isWin);
+
+		const resultEl = this.el.querySelector('.result');
+
+		// 이겼으면 파란색, 졌으면 빨간색 텍스트 테두리를 설정
+		if (isWin) {
+            resultEl.style.textShadow = '-1px 1px 0px #0000ff, 1px -1px 0px #0000ff, -1px -1px 0px #0000ff, 1px 1px 0px #0000ff';
+        } else {
+            resultEl.style.textShadow = '-1px 1px 0px #ff0000, 1px -1px 0px #ff0000, -1px -1px 0px #ff0000, 1px 1px 0px #ff0000'; ;
+        }
+
 	}
 }
