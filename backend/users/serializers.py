c6e511e64718526e480c6736accb26de58949876
nuyheat <taehkim2@student.42seoul.kr>
2024-09-06 01:30:32 +0900

    refactor: change LoginSerializer and add extra_kwargs to JoinSerializer
    [extra_kwargs] add profile_img required

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,20 +3,24 @@ from django.contrib.auth import authenticate
 from .models import User
 
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(help_text="Username used as the login ID for the user.")
-    password = serializers.CharField(help_text="Password for the user login.")
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
 
     def validate(self, data):
         user = authenticate(username=data['username'], password=data['password'])
         if user is None:
-            raise serializers.ValidationError("username 또는 password가 잘못 되었습니다.")
+            return
         return {'user': user}
 
-class SignUpSerializer(serializers.ModelSerializer):
+class JoinSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'profile_img']
+        extra_kwargs = {
+            'profile_img': {'required': True}
+        }
     
     def save(self):
         user = User.objects.create_user(
