75f235567cb111851b7a19deb0b4806da35f5daf
nuyheat <taehkim2@student.42seoul.kr>
2024-09-08 13:43:09 +0900

    chore: add validate and save to OAuthLoginSerializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -35,6 +35,32 @@ class OAuthLoginSerializer(serializers.Serializer):
         ## request의 fieldvalue가 비어있는 경우
         
         return super().to_internal_value(data)
+    def validate(self, data):
+        user = self.instance
+        if user is None:
+            raise serializers.ValidationError(
+                {
+                    "error_code": 400,
+                    "detail": "존재하지 않는 username 또는 password 입니다"
+                }
+            )
+        ## request와 일치하는 유저정보가 없는 경우
+        
+        if user.status != User.STATUS_MAP['오프라인']:
+            raise serializers.ValidationError(
+                {
+                    "error_code": 409,
+                    "detail": "다른 기기에서 이미 로그인되어 있습니다"
+                }
+            )
+        ## 중복 로그인을 시도하는 경우
+        
+        return {'user': user}
+    def save(self):
+        user = self.validated_data['user']
+        user.status = User.STATUS_MAP['온라인']
+        user.save()
+        return user
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150, required=True)
