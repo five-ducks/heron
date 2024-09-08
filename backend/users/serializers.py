56c50b85f599a57d5be9e85773bb2e9c428df8a8
nuyheat <taehkim2@student.42seoul.kr>
2024-09-08 15:08:32 +0900

    chore: delete code detail comment

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,15 +2,6 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import User
 
-## serializers.ValidationError로 설정한 fieldname과 fieldvalue는 serializer.errors.get(fieldname)으로 외부에서 확인할 수 있습니다.
-## 하지만 serializer.errors.get(key)을 통해 반환된 fieldvalue는 errordetail객체로 반환됩니다.
-## errordetail객체는 내가 설정한 value 외의 추가적인 필드를 생성하여 담고 있습니다.
-## 따라서 내가 설정한 fieldvalue를 사용하고 싶다면 int 또는 str로 변환해서 사용하면 됩니다.
-
-##### ValidationError의 차이 #####
-## to_internal_value의 경우 dict의 fieldvalue를 errordetail객체로 감싸게 됩니다.
-## validate의 경우 fieldvalue를 errordetail객체로 감싸고 list로 한번 더 감싸게 됩니다.
-
 class OAuthLoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150, required=True)
     
@@ -89,11 +80,6 @@ class LoginSerializer(serializers.Serializer):
         ## request의 fieldvalue가 비어있는 경우
         
         return super().to_internal_value(data)
-        ## to_internal_value를 오버라이딩 하여 커스첨 validation check를 진행합니다.
-        ## 이후 부모 클래스의 to_internal_value를 호출합니다.
-    ## to_internal_value는 is_valid()를 진행할 때 validate()가 호출되기 전 호출되는 함수입니다.
-    ## 해당 함수를 오버라이딩 하여 required한 username, password의 valid check를 커스텀 할 수 있습니다.
-        
     def validate(self, data):
         user = authenticate(username=data['username'], password=data['password'])
         if user is None:
