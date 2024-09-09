f49c73f0eed8af5b0ab00d04df19ff5ba63b912c
nuyheat <taehkim2@student.42seoul.kr>
2024-09-10 08:13:54 +0900

    chore: delete match serializer and views

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MatchViewSet, TournamentViewSet
+from .views import MatchViewSet
 
 router = DefaultRouter()
 router.register(r'', MatchViewSet, basename='match')
-router.register(r'', TournamentViewSet, basename='tournament')
 
 urlpatterns = [
     path('', include(router.urls)),
