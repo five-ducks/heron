195fe821cb39fa1e8cf741e9a2df07f4044b2909
Siha Lee <mlice1030@soongsil.ac.kr>
2024-10-09 20:25:51 +0900

    Frontend mypage user api (#26)
    * feat: connect withdrawalBtn, saveBtn with backed APIs
    
    * feat: connect loadGameRecords with backed API(GET users/self)
    
    * refactor: Improve level calculation in ProfileLevel component
    
    - uses a while loop to iterate through the exp and calculate the level and exp required for the next level.

--- frontend/src/store/game.js
+++ frontend/src/store/game.js
@@ -10,8 +10,18 @@ export default store;
 
 export const loadGameRecords = async () => {
 	try {
-		const response = await fetch('https://localhost/src/temp/gameRecords.json');
-		store.state.gameRecords = await response.json();
+		const response = await fetch('/api/users/self');
+		if (response.status === 200)
+		{
+			const user = await response.json();
+			store.state.gameRecords = user.matches
+		}
+		else if (response.status === 403)
+		{
+			console.error('Forbidden:', response.error);
+		}
+		else
+			console.error('Unknown error:', response.error);
 	} catch (error) {
 		console.error('Error loading game records:', error);
 	}
