7e43b32610f30c40b09ed00b13ecf07cfaeeaac4
mingkyeongg <ming0820@naver.com>
2024-10-09 19:59:25 +0900

    feat: 모달 외부 클릭 방지

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -20,21 +20,26 @@ export class Modal extends Component {
 			this.close();
 		});
 
-		window.addEventListener('click', (event) => {
-			if (event.target === this.el) {
-				this.close();
-			}
+		this.el.addEventListener('click', (event) => {
+			event.stopPropagation(); // 모달 외부 클릭 무시
+		});
+
+		this.el.querySelector('.modal-content').addEventListener('click', (event) => {
+			event.stopPropagation();
 		});
 	}
 
 	open() {
 		this.el.style.display = 'block';
+		document.body.style.pointerEvents = 'none';
+		this.el.style.pointerEvents = 'auto';
 		history.pushState(null, "", location.href);
 		window.addEventListener("popstate", this.preventGoBack);
 	}
 
 	close() {
 		this.el.style.display = 'none';
+		document.body.style.pointerEvents = 'auto';
 		window.removeEventListener("popstate", this.preventGoBack);
 		this.onClose();
 		this.el.remove();
