8c4012f453c231e52fea8501dec815e3ece4b132
sunsetfactory <yang1194us@gmail.com>
2024-11-05 13:16:10 +0900

    chore: modal architecture

--- frontend/src/components/InfoFriendModal/InfoFriendModal.js
+++ frontend/src/components/InfoFriendModal/InfoFriendModal.js
@@ -2,27 +2,53 @@ import { Modal } from "../Modal/index.js";
 import { FriendGameRecords } from "../FriendGameRecords/FriendGameRecords.js";
 
 export class InfoFriendModal extends Modal {
-    constructor(onClose = () => {}) {
+    constructor(props, onClose) {
+        console.log(props);
+        const name = props.username;
         const content = /*html*/`
-            <h1 class="title">친구 정보</h1>
             <div class="friend_info">
-                <div class="friend_name">이름: 홍길동</div>
-                <div class="friend_level">레벨: 5</div>
+                <div class="friend_name">이름: ${name}</div>
+                <div class="friend_level">레벨: 0</div>
                 <div class="friend_winrate">승률: 75%</div>
             </div>
             <div class="info_record"></div>
         `;
-        super(content, onClose);
+        super('친구 정보', content, onClose);
+        this.userinfo = props;
 
         this.el.classList.add("info_friend_modal");
 
         const matchRecordList = this.el.querySelector(".info_record");
         const gameRecords = new FriendGameRecords();
-        gameRecords.render();
+        gameRecords.render(this.userinfo.matches, this.userinfo.username, this.userinfo.profile_img);// 
         matchRecordList.appendChild(gameRecords.el);
 
 
         this.render();
+    }
+
+    // static getContent() {
+    //     return /*html*/`
+    //         <div class="friend_info">
+    //             <div class="friend_name">이름: </div>
+    //             <div class="friend_level">레벨: </div>
+    //             <div class="friend_win
+    //         </div>
+    //         <div class="info_record"></div>
+    //     `;
+    // }
+
+    calculateLevel(exp) {
+        let level = 1;
+        let expForNextLevel = ProfileLevel.BASE_EXP; // 클래스 변수 접근 시 ProfileLevel 사용
+
+        while (exp >= expForNextLevel) {
+            exp -= expForNextLevel; // 다음 레벨로 가기 위해 필요한 exp 차감
+            level++; // 레벨 업
+            expForNextLevel = Math.floor(expForNextLevel * ProfileLevel.EXP_MULTIPLIER); // 다음 레벨에 필요한 exp 증가
+        }
+
+        return level;
     }
 
     render() {
