97732a7e5ea0462431af53532d4ee1641340e48b
nuyheat <taehkim2@student.42seoul.kr>
2024-11-10 00:25:07 +0900

    refactor: Refactoring docker-compose and dockerfile

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/env sh
-# python manage.py makemigrations chat games profiles notice
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || true
-python manage.py shell -c "from users.models import User; user=User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()"
+python manage.py shell -c "
+from django.contrib.auth import get_user_model; 
+User = get_user_model(); 
+if not User.objects.filter(username='$DJANGO_SUPERUSER_NAME').exists(): 
+    user = User.objects.create_superuser(username='$DJANGO_SUPERUSER_NAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD'); 
+"
 python manage.py clearsessions
-exec python manage.py runserver 0.0.0.0:8000
+
+exec dumb-init python manage.py runserver 0.0.0.0:8000
